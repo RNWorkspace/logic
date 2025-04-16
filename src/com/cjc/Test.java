fa2628de427adc6d31f1cdd4a82525c6fdad7e7e
Rushikesh <Rushikesh@DESKTOP-CGJM1LP>
2025-04-16 12:41:50 +0530

    con add

--- src/com/cjc/Test.java
+++ src/com/cjc/Test.java
@@ -1,6 +1,9 @@
 package com.cjc;
 
 public class Test {
+	public Test() {
+		System.out.println("constructor");
+	}
 	public static void main(String[] args) {
 		System.out.println("Hello word");
 	}
